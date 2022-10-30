372ad415e1ba8854b2c8911368683e26f6139ec1
aliberkayberber <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>
2022-10-30 22:02:05 +0300

    Second Commit

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 
 class ViewController: UIViewController, MKMapViewDelegate , CLLocationManagerDelegate{
 
     
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
+    
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,15 +36,65 @@ class ViewController: UIViewController, MKMapViewDelegate , CLLocationManagerDel
         locationManager.startUpdatingLocation()
         
         
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
     }
 
+    @objc func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .began {
+            
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            
+            let touchCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            
+            chosenLatitude = touchCoordinates.latitude
+            chosenLongitude = touchCoordinates.longitude
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchCoordinates
+            annotation.title = nameText.text
+            annotation.subtitle = commentText.text
+            self.mapView.addAnnotation(annotation)
+            
+        }
+        
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
         
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
     }
+    
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        newPlace.setValue(nameText.text, forKey: "title")
+        newPlace.setValue(commentText.text, forKey: "subtitle")
+        newPlace.setValue(chosenLatitude, forKey: "latitude")
+        newPlace.setValue(chosenLongitude, forKey: "longitude")
+        
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            print("succes")
+        } catch {
+            print("error")
+        }
+        
+    }
+    
     
 }
 
